acc7adb06f7318c8440e4e47e445eda8c5a982be
Greg Babiars <gbabiars@gmail.com>
2013-03-16 00:48:29 -0500

    Modified redis for Heroku

--- data/teams-loader.js
+++ data/teams-loader.js
@@ -2,7 +2,15 @@ var moment = require('moment'),
     _ = require('underscore'),
     espnApi = require('./espn-api'),
     redis = require('redis'),
+    redisClient;
+
+if(process.env.REDISTOGO_URL) {
+    var rtg   = require('url').parse(process.env.REDISTOGO_URL);
+    redisClient = redis.createClient(rtg.port, rtg.hostname);
+    redisClient.auth(rtg.auth.split(':')[1]);
+} else {
     redisClient = redis.createClient();
+}
 
 var convertToTeams = function(data) {
     return data.sports[0].leagues[0].teams

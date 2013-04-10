02e859ffd85c68bfd454812fb84cb8f38a30e5d8
Greg Babiars <gbabiars@gmail.com>
2013-04-09 22:09:27 -0500

    Day stats have custom data viz
    No longer using nvd3.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -129,42 +129,83 @@ App.ConferencesByMonthController = Em.ArrayController.extend({
 
 App.TeamsStatsView = Em.View.extend({
     contentChanged: function() {
-        var data,
-            options,
-            height;
-
         if(this.get('controller.content')) {
-            data = this.get('controller.content').map(function(d) {
-                return {
-                    label: d.get('displayName'),
-                    value: d.get('count')
-                };
+            var data = this.controller.get('content');
+
+            var containerWidth = this.$('.container').width();
+
+            var margin = {top: 10, right: 20, bottom: 10, left: 180},
+                width = containerWidth - margin.left - margin.right,
+                height = 7200 - margin.top - margin.bottom;
+
+            var max = d3.max(data, function(d) {
+                return d.count;
             });
-            options = [{
-                key: '# of stories',
-                color: '#1f77b4',
-                values: data
-            }];
-            height = data.length * 20 + 200;
-            this.$().find('#chart svg').css({ height: height + 'px' });
-            nv.addGraph(function() {
-                var chart = nv.models.multiBarHorizontalChart()
-                    .x(function(d) { return d.label })
-                    .y(function(d) { return d.value })
-                    .margin({ top: 30, right: 20, bottom: 50, left: 250 })
-                    .showValues(true)
-                    .tooltips(false)
-                    .showControls(false);
 
-                d3.select('#chart svg')
-                    .datum(options)
-                    .transition().duration(500)
-                    .call(chart);
+            var x = d3.scale.linear()
+                .domain([0, max])
+                .range([0, width])
+                .nice();
 
-                nv.utils.windowResize(chart.update);
+            var y = d3.scale.ordinal()
+                .domain(d3.range(data.length))
+                .rangeBands([0, height], .2);
 
-                return chart;
-            });
+            var svg = d3.select('#chart').append('svg')
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+
+            var chartContainer = svg
+                .append('g')
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            chartContainer.selectAll(".bar")
+                .data(data)
+                .enter().append("rect")
+                .attr("class", "bar")
+                .attr("x", function() { return x(0); })
+                .attr("y", function(d, i) { return y(i); })
+                .attr("height", y.rangeBand())
+                .transition()
+                .duration(300)
+                .attr("width", function(d) { return x(d.count); });
+
+            chartContainer
+                .append('g')
+                .selectAll("text")
+                .data(data)
+                .enter().append("text")
+                .attr("x", function(d) { return x(d.count); })
+                .attr("y", function(d, i) { return y(i) + y.rangeBand() / 2; })
+                .attr("dx", 3) // padding-right
+                .attr("dy", ".35em") // vertical-align: middle
+                .attr("text-anchor", "start") // text-align: right
+                .text(function(d) { return d.count; })
+                .attr('visibility', 'hidden')
+                .transition()
+                .delay(300)
+                .attr('visibility', 'visible');
+
+            chartContainer
+                .append('g')
+                .attr('class', 'axis')
+                .selectAll("text")
+                .data(data)
+                .enter().append("text")
+                .attr("x", 0)
+                .attr("y", function(d, i) { return y(i) + y.rangeBand() / 2; })
+                .attr("dx", -3) // padding-right
+                .attr("dy", ".35em") // vertical-align: middle
+                .attr("text-anchor", "end") // text-align: right
+                .text(function(d) { return d.location; });
+
+            chartContainer.append("g")
+                .attr("class", "y axis")
+                .append("line")
+                .attr("x1", x(0))
+                .attr("x2", x(0))
+                .attr("y1", 0)
+                .attr("y2", height);
         }
     }.observes('controller.content')
 });
